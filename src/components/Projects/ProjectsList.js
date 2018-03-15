06fa25469d59fbf9eabcbb49996eba9585c0df4a
Jeff Moura <jefferson@dimoura.com>
2018-03-15 17:25:27 -0300

    created tests + api call for project list

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProjectsList extends Component {
 
-data_projects = [{
+  data_projects = [{
     id: 1,
     name: 'Home',
     status: 'On Going',
@@ -64,11 +65,24 @@ data_projects = [{
     projects: this.data_projects
   }
 
+  getProjectList = async () =>{
+    const api_call = await fetch('http://localhost:8080/controllers/projects/controller.php');
+    const data = await api_call.json();
+
+    this.setState({
+      projects: data.response
+    });
+  }
+
+  componentDidMount(){
+    this.getProjectList();
+  }
 
   filterTaskByStatus = (task, status) =>{
     var _temp = 0;
 
     for(var task_status in task){
+      // console.info(task.hasOwnProperty);
       if(task[task_status].status === status){
         _temp = task[task_status].total;
         break;
@@ -82,6 +96,20 @@ data_projects = [{
       <div className="col-sm-9">
         <div className="wrap-design">
 
+          <div className="panel panel-info">
+            <Link className="btn-on-panel" to="/projects/new">
+              <span className="btn-icon">+</span>
+              Criar Novo Projeto
+            </Link>
+
+            <Link className="btn-on-panel active" to="/projects/new">
+              Projetos Em Desenvolvimento
+            </Link>
+
+            <Link className="btn-on-panel" to="/projects/new">
+              Projetos Finalizados
+            </Link>
+          </div>
           
           <div className="panel panel-default">
             <table className="table">
@@ -99,12 +127,12 @@ data_projects = [{
               <tbody>
               {this.state.projects.map((item, i) =>
                 <tr data-percent="80%" className="developing-projects" key={i}>
-                  <td className="text-center"><i className="glyphicon glyphicon-pencil"></i></td>
-                  <td width="30%">{item.name}</td>
-                  <td width="100px">{item.status}</td>
-                  <td className="text-center task-done">{this.filterTaskByStatus(item.tasks, 'Done')}</td>
-                  <td className="text-center task-testing">{this.filterTaskByStatus(item.tasks, 'Testing')}</td>
-                  <td className="text-center test-developping">{this.filterTaskByStatus(item.tasks, 'Developing')}</td>
+                  <td className="text-center"><Link to={`/project/edit/${item.id}`}><i className="glyphicon glyphicon-pencil"></i></Link></td>
+                  <td width="30%"><Link to={`/project/${item.id}/tasks`}>{item.name}</Link></td>
+                  <td width="100px"><Link to={`/project/${item.id}/tasks`}>{item.status}</Link></td>
+                  <td className="text-center task-done"><Link to={`/project/${item.id}/tasks`}>{this.filterTaskByStatus(item.tasks, 'Done')}</Link></td>
+                  <td className="text-center task-testing"><Link to={`/project/${item.id}/tasks`}>{this.filterTaskByStatus(item.tasks, 'Testing')}</Link></td>
+                  <td className="text-center test-developping"><Link to={`/project/${item.id}/tasks`}>{this.filterTaskByStatus(item.tasks, 'Developing')}</Link></td>
                   <td className="developing-projects__users">
                     {item.users.map((user, j) =>
                       <p key={j} className={user.role} data-isonline={user.isOnline ? 'Online':'Offline'}>{user.name}<span></span></p>
@@ -114,7 +142,7 @@ data_projects = [{
               )}
               </tbody>
             </table>
-          </div>   
+          </div>  
         </div>   
       </div>
     );
